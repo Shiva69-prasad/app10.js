05ff9106bcd867b8ddd22a383d336166f7286a49
Shiva69-prasad <shiva31694@gmail.com>
2025-08-24 20:16:54 +0530

    app10.js

---
+++ app10.js
@@ -0,0 +1,45 @@
+// LOOPS IN JS
+
+  console.log("hii")
+  console.log("hii")
+  console.log("hii")
+  console.log("hii")
+  console.log("hii")
+
+  //FOR LOOP
+  for(let i=0;i<10;i++){
+      console.log("shiva{i}")
+  }
+
+  const fruits=["apple","banana","guva","grappes"]
+  for(i=0;i<fruits.length;i++){
+      console.log(fruits[i])
+  } 
+
+//FOR OF LOOPS 
+ const peoples=["shiva","govardhan","manohar","sumanth"]
+ for(A of peoples){
+     console.log(A)
+ }
+
+// WHILE LOOP
+    let i=1;
+  while(i<10){
+      console.log(i)
+      i++;
+  }
+
+//DO WHILE LOOP
+     let i=1;
+   do{
+       i++;
+       console.log(i)
+   }
+   while(i<10)
+
+// FOR IN LOOP
+  const details={name:"shiva",age:20}
+
+  for(x in details){
+      console.log(details [x])
+  }
